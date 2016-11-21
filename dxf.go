4dce06be36c794c74ed65572b752685889273f96
ma2ma <steve2008.ma@gmail.com>
2016-11-21 21:30:44 +0800

    add open io.reader function

--- dxf.go
+++ dxf.go
@@ -3,11 +3,13 @@ package dxf
 import (
 	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strings"
+
 	"github.com/ma2ma/godxf/color"
 	"github.com/ma2ma/godxf/drawing"
 	"github.com/ma2ma/godxf/table"
-	"os"
-	"strings"
 )
 
 // Default values.
@@ -19,6 +21,81 @@ var (
 // NewDrawing creates a drawing.
 func NewDrawing() *drawing.Drawing {
 	return drawing.New()
+}
+
+// Open opens the named DXF file.
+func OpenReader(r io.Reader) (*drawing.Drawing, error) {
+	var err error
+	//defer r.Close()
+	scanner := bufio.NewScanner(r)
+	d := NewDrawing()
+	var code, value string
+	parsers := []func(*drawing.Drawing, int, [][2]string) error{
+		ParseHeader,
+		ParseClasses,
+		ParseTables,
+		ParseBlocks,
+		ParseEntities,
+		ParseObjects,
+	}
+	data := make([][2]string, 0)
+	setparser := false
+	var parser func(*drawing.Drawing, int, [][2]string) error
+	line := 0
+	startline := 0
+	for scanner.Scan() {
+		line++
+		if line%2 == 1 {
+			code = strings.TrimSpace(scanner.Text())
+			if err != nil {
+				return d, err
+			}
+		} else {
+			value = scanner.Text()
+			if setparser {
+				if code != "2" {
+					return d, fmt.Errorf("line %d: invalid group code: %s", line, code)
+				}
+				ind := drawing.SectionTypeValue(strings.ToUpper(value))
+				if ind < 0 {
+					return d, fmt.Errorf("line %d: unknown section name: %s", line, value)
+				}
+				parser = parsers[ind]
+				startline = line + 1
+				setparser = false
+			} else {
+				if code == "0" {
+					switch strings.ToUpper(value) {
+					case "EOF":
+						return d, nil
+					case "SECTION":
+						setparser = true
+					case "ENDSEC":
+						err := parser(d, startline, data)
+						if err != nil {
+							return d, err
+						}
+						data = make([][2]string, 0)
+						startline = line + 1
+					default:
+						data = append(data, [2]string{code, scanner.Text()})
+					}
+				} else {
+					data = append(data, [2]string{code, scanner.Text()})
+				}
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return d, err
+	}
+	if len(data) > 0 {
+		err := parser(d, startline, data)
+		if err != nil {
+			return d, err
+		}
+	}
+	return d, nil
 }
 
 // Open opens the named DXF file.
